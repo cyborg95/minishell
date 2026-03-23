e5751313c6bbd02fd46050e0c943cb8091091430
Willynga6 <ton_email_github@exemple.com>
2026-03-23 13:58:06 +0100

    Petit fix de compilation

--- src/redir_error.c
+++ src/redir_error.c
@@ -6,7 +6,7 @@
 /*   By: wngambi <wngambi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/23 09:28:26 by wngambi           #+#    #+#             */
-/*   Updated: 2026/03/23 12:56:55 by wngambi          ###   ########.fr       */
+/*   Updated: 2026/03/23 13:31:28 by wngambi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,13 +49,13 @@ bool	last_token_is_redir(t_token *lst_token)
 		return (false);
 	while (lst_token->next != NULL)
 		lst_token = lst_token->next;
-	if ((lst_token->type == REDIR_IN))
+	if (lst_token->type == REDIR_IN)
 		return (true);
-	if ((lst_token->type == REDIR_OUT))
+	if (lst_token->type == REDIR_OUT)
 		return (true);
-	if ((lst_token->type == HEREDOC))
+	if (lst_token->type == HEREDOC)
 		return (true);
-	if ((lst_token->type == APPEND))
+	if (lst_token->type == APPEND)
 		return (true);
 	return (false);
 }
