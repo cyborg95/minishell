50eb9e38a12e0a72fcf3e2e2dcf30b979d3fb67f
Willynga6 <ton_email_github@exemple.com>
2026-04-17 06:46:05 +0200

    s

--- src/src_parser/parsing_minishell.c
+++ src/src_parser/parsing_minishell.c
@@ -6,7 +6,7 @@
 /*   By: wngambi <wngambi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 07:10:12 by wngambi           #+#    #+#             */
-/*   Updated: 2026/04/14 08:05:24 by wngambi          ###   ########.fr       */
+/*   Updated: 2026/04/17 06:41:54 by wngambi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,31 @@ bool	read_prompt(char **line, t_malloc **malloc_lst)
 
 /*	=====================================================	*/
 
+static void	free_token_lst(t_token *token_lst, t_malloc **lst_malloc)
+{
+	t_token	*tmp;
+	bool	end_of_lst;
+
+	if (!token_lst)
+		return ;
+	end_of_lst = false;
+	while (token_lst)
+	{
+		if (token_lst->next)
+			tmp = token_lst->next;
+		else
+			end_of_lst = true;
+		free_remix (token_lst, lst_malloc);
+		if (end_of_lst)
+			break ;
+		else
+			token_lst = tmp;
+	}
+	token_lst = NULL;
+}
+
+/*	=====================================================	*/
+
 t_cmd	*parse_input(char *line, t_malloc **lst_malloc)
 {
 	t_token		*token_list;
@@ -70,9 +95,14 @@ t_cmd	*parse_input(char *line, t_malloc **lst_malloc)
 	init_token_lst (&token_list, lst_malloc, &line);
 	if (!check_token_lst (&token_list, PROMPT, &line, lst_malloc))
 		return (NULL);
-	//display_token (token_list);
 	init_command_lst (token_list, lst_malloc, &cmd_lst);
+	display_token (token_list);
+	free_token_lst (token_list, lst_malloc);
+	if (token_list)
+		display_token (token_list);
 	return (cmd_lst);
 }
 
 /*	=====================================================	*/
+
+
